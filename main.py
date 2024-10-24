670285e161269768807a177b2c632aa33d328307
Pedro Quintas <pedroquintas1213@gmail.com>
2024-10-24 16:25:28 -0300

    v2.2.0
    alterações em teste

--- main.py
+++ main.py
@@ -11,12 +11,11 @@ from send_whatsapp import enviar_mensagem_whatsapp
 from uploud_To_S3 import upload_html_to_s3
 from processo_data import fetch_numero
 from processo_data import fetch_email
-from db_conexão import get_db_connection
-from db_conexão import get_db_ligcontato
+from processo_data import fetch_companies
+from processo_data import status_envio
 
 def enviar_emails():
 
-    
     data_do_dia = datetime.now()
 
     # Busca os dados dos clientes e processos
@@ -25,24 +24,15 @@ def enviar_emails():
     contador_Inativos = 0
 
     total_escritorios = len(clientes_data)  
+    #recupera dos dados do comapanies
+    config = fetch_companies()
 
-    try:
-        db_connection = get_db_connection()
-        db_cursor = db_connection.cursor()
-
-        # Puxar dados de configuração do SMTP
-        db_cursor.execute("""SELECT ID_lig, url_Sirius, sirius_Token,aws_s3_access_key,aws_s3_secret_key,bucket_s3,smtp_host, smtp_port, smtp_username, smtp_password, smtp_from_email, 
-                          smtp_from_name,smtp_reply_to, smtp_cc_emails,smtp_bcc_emails,smtp_envio_test, url_thumbnail_whatsapp, url_thumbnail FROM companies""")
-        config = db_cursor.fetchone()
-
-        if config:
-            ID_lig,url_Sirius,sirius_Token,aws_s3_access_key,aws_s3_secret_key,bucket_s3,smtp_host, smtp_port, smtp_user,smtp_password,smtp_from_email,smtp_from_name,smtp_reply_to,smtp_cc_emails,smtp_bcc_emails,smtp_envio_test,whatslogo,logo = config
-        else:
+    if config:
+            ID_lig,url_Sirius,sirius_Token,aws_s3_access_key,aws_s3_secret_key,bucket_s3,smtp_host, smtp_port, smtp_user,smtp_password,smtp_from_email,smtp_from_name,smtp_reply_to,smtp_cc_emails,smtp_bcc_emails,whatslogo,logo = config
+    else:
             logger.warning("Configuração SMTP não encontrada.")
             exit()
-    except mysql.connector.Error as err:
-        logger.error(f"Erro ao conectar ao banco de dados: {err}")
-        exit()
+
 
     # Configuração do SMTP
     smtp_config = (smtp_host, smtp_port, smtp_user, smtp_password,smtp_from_email,smtp_from_name,smtp_reply_to,smtp_cc_emails,smtp_bcc_emails,logo)
@@ -62,70 +52,52 @@ def enviar_emails():
         localizador = str(uuid.uuid4()) 
 
         email_body = generate_email_body(cliente, processos, logo, localizador, data_do_dia)
-        email_receiver =  smtp_envio_test #processos[0]['emails']
+        email_receiver = processos[0]['emails']
         bcc_receivers = smtp_bcc_emails
         cc_receiver = smtp_cc_emails
         subject = f"LIGCONTATO - DISTRIBUIÇÕES {data_do_dia.strftime('%d/%m/%y')} - {cliente}"
 
-        try:
+        # Envia o e-mail
+        send_email(smtp_config, email_body, email_receiver, bcc_receivers,cc_receiver, subject)
 
-            # Envia o e-mail
-            send_email(smtp_config, email_body, email_receiver, bcc_receivers,cc_receiver, subject)
+        # Gera e faz o upload do arquivo HTML para o S3
+        object_name = f"{cod_cliente}/{data_do_dia.strftime('%d-%m-%y')}/{localizador}.html"
+        permanent_url = upload_html_to_s3(email_body, bucket_s3, object_name, aws_s3_access_key, aws_s3_secret_key)
 
-            # Gera e faz o upload do arquivo HTML para o S3
-            object_name = f"{cod_cliente}/{data_do_dia.strftime('%d-%m-%y')}/{localizador}.html"
-            permanent_url = upload_html_to_s3(email_body, bucket_s3, object_name, aws_s3_access_key, aws_s3_secret_key)
+        #verifica se o cliente tem numero para ser enviado
+        if not cliente_number:
+            logger.warning(f"Cliente: '{cod_cliente}' não tem número cadastrado na API")
+        else:
+            for numero in cliente_number:
+                #envia a mensagem via whatsapp
+                enviar_mensagem_whatsapp(ID_lig,
+                                        url_Sirius,
+                                        sirius_Token,
+                                        numero['numero'],
+                                        permanent_url,
+                                        f"Distribuição de novas ações - {cliente}",
+                                        f"Total: {len(processos)} publicações",
+                                        whatslogo
+                                        )
 
-            #verifica se o cliente tem numero para ser enviado
-            # if not cliente_number:
-            #     logger.warning(f"Cliente: '{cod_cliente}' não tem número cadastrado na API")
-            # else:
-            #     for numero in cliente_number:
-            #         #envia a mensagem via whatsapp
-            #         enviar_mensagem_whatsapp(ID_lig,
-            #                                 url_Sirius,
-            #                                 sirius_Token,
-            #                                 numero['numero'],
-            #                                 permanent_url,
-            #                                 f"Distribuição de novas ações - {cliente}",
-            #                                 f"Total: {len(processos)} publicações",
-            #                                 whatslogo
-            #                                 )
-
-            logger.info(f"""E-mail enviado para {cliente} às {datetime.now().strftime('%H:%M:%S')} - Total de processos: {len(processos)}
-                            numeros: {','.join(cliente_number[0] for cliente_number in cliente_number)}\n---------------------------------------------------""")
+        logger.info(f"""E-mail enviado para {cliente} às {datetime.now().strftime('%H:%M:%S')} - Total de processos: {len(processos)}
+                        numeros: {','.join(cliente['numero'] for cliente in cliente_number)}\n---------------------------------------------------""")
 
 
-            for processo in processos:
-                processo_id = processo['ID_processo']  
-                #db_cursor.execute("UPDATE processo SET status = 'S' WHERE ID_processo = %s", (processo_id,))
-                db_cursor.execute("""INSERT INTO envio_emails (ID_processo, numero_processo, 
-                                  cod_escritorio, localizador_processo, data_envio, localizador, email_envio, numero_envio, link_s3, data_hora_envio)
-                                  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s,%s)""",
-                                    (processo_id,   
-                                    processo['numero_processo'],
-                                    processo['cod_escritorio'],
-                                    processo['localizador'], 
-                                    data_do_dia.strftime('%Y-%m-%d'),
-                                    localizador, 
-                                    email_receiver,
-                                    ','.join(cliente_number[0] for cliente_number in cliente_number),
-                                    permanent_url,
-                                    datetime.now()))
-
-            db_connection.commit()  # Commit all changes
-   
-        except mysql.connector.Error as err:
-            logger.error(f"Erro ao atualizar o status ou registrar o envio: {err}")
+        for processo in processos:
+            processo_id = processo['ID_processo']
+            status_envio(processo_id,processo['numero_processo'],processo['cod_escritorio'],processo['localizador'],
+                            data_do_dia.strftime('%Y-%m-%d'),localizador,email_receiver, cliente_number,permanent_url)
+        
 
     logger.info(f"\nEnvio finalizado, total de escritorios enviados: {total_escritorios - contador_Inativos}")
 
+# Atualiza a exibição
 def Atualizar_lista_pendetes():
     
         clientes_data = fetch_processes_and_clients()
         total_escritorios = len(clientes_data)  
         total_processos_por_escritorio = {cliente: len(processos) for cliente, processos in clientes_data.items()}
-        # Atualiza a exibição
         logger.info(f"\nAguardando o horário de envio... (Atualizado: {datetime.now().strftime('%d-%m-%y %H:%M')})")
         logger.info(f"Total de escritórios a serem enviados: {total_escritorios}")
         for cliente, total_processos in total_processos_por_escritorio.items():
@@ -134,22 +106,16 @@ def Atualizar_lista_pendetes():
 
 
 # atualiza a lista de pendentes:
+#schedule.every().hour.do(Atualizar_lista_pendetes)
 
-#schedule.every(30).minutes.do(Atualizar_lista_pendetes)
-
-# Agenda o envio para todos os dias às 16:00
+# # Agenda o envio para todos os dias às 16:00
 #schedule.every().day.at("16:00").do(enviar_emails)
 
 if __name__ == "__main__":
 
     enviar_emails()
     # Atualizar_lista_pendetes()
-    
+
     # while True:
-    #     schedule.run_pending()
+    #     schedule.run_pending()  # Executa as tarefas agendadas
     #     time.sleep(1)
-
-    
-
-
-
--- processo_data.py
+++ processo_data.py
@@ -7,7 +7,7 @@ import os
 from db_conexão import get_db_connection
 from db_conexão import get_db_ligcontato
 
-
+#captura todos os dados do processo
 def fetch_processes_and_clients():
 
 
@@ -83,77 +83,90 @@ def process_result(result, clientes_data):
         'cliente_status' : status_cliente
     })
 
+#captura os links do processo
 def fetch_links(process_id):
-    db_connection = get_db_connection()
-    db_cursor = db_connection.cursor()
-    
-    querylinks = "SELECT * FROM apidistribuicao.processo_docinicial WHERE ID_PROCESSO = %s AND doc_peticao_inicial= 0"
-    db_cursor.execute(querylinks, (process_id,))
-    results_links = db_cursor.fetchall()
+    try:
+        db_connection = get_db_connection()
+        db_cursor = db_connection.cursor()
+        
+        querylinks = "SELECT * FROM apidistribuicao.processo_docinicial WHERE ID_PROCESSO = %s AND doc_peticao_inicial= 0"
+        db_cursor.execute(querylinks, (process_id,))
+        results_links = db_cursor.fetchall()
 
-    links_list = []
-    for links in results_links:
-        id_link = links[1]
-        link_doc = links[2]
-        tipoLink = links[3]
-        links_list.append({'link_doc': link_doc,
-                            'tipoLink': tipoLink,
-                              'id_link': id_link})
+        links_list = []
+        for links in results_links:
+            id_link = links[1]
+            link_doc = links[2]
+            tipoLink = links[3]
+            links_list.append({'link_doc': link_doc,
+                                'tipoLink': tipoLink,
+                                'id_link': id_link})
 
-    db_cursor.close()
-    db_connection.close()
+        db_cursor.close()
+        db_connection.close()
 
-    return links_list
+        return links_list
+    except mysql.connector.Error as err:
+        logger.error(f"erro na consulta do documento inicial: {err}")
 
+#captura o autor pelo ID do processo
 def fetch_autor(process_id):
-    db_connection = get_db_connection()
-    db_cursor = db_connection.cursor()
-    
-    queryautor = "SELECT * FROM apidistribuicao.processo_autor WHERE ID_processo = %s"
-    db_cursor.execute(queryautor, (process_id,))
-    results_autor = db_cursor.fetchall()
+    try:
+        db_connection = get_db_connection()
+        db_cursor = db_connection.cursor()
+        
+        queryautor = "SELECT * FROM apidistribuicao.processo_autor WHERE ID_processo = %s"
+        db_cursor.execute(queryautor, (process_id,))
+        results_autor = db_cursor.fetchall()
 
-    autor_list= []
-    for autores in results_autor:
-        ID_autor = autores[1]
-        Cod_Polo = autores[2]
-        nome = autores[3]
-        autor_list.append({'id_autor':ID_autor,
-                           'cod_polo':Cod_Polo,
-                           'nomeAutor':nome})
-    db_cursor.close()
-    db_connection.close()
+        autor_list= []
+        for autores in results_autor:
+            ID_autor = autores[1]
+            Cod_Polo = autores[2]
+            nome = autores[3]
+            autor_list.append({'id_autor':ID_autor,
+                            'cod_polo':Cod_Polo,
+                            'nomeAutor':nome})
+        db_cursor.close()
+        db_connection.close()
 
-    return autor_list
+        return autor_list
+    except mysql.connector.Error as err:
+        logger.error(f"erro na consulta do Autor: {err}")    
 
-
+#captura o reu pelo ID do processo
 def fetch_reu(process_id):
-    db_connection = get_db_connection()
-    db_cursor = db_connection.cursor()
-    
-    queryreu = "SELECT * FROM apidistribuicao.processo_reu WHERE ID_processo = %s"
-    db_cursor.execute(queryreu, (process_id,))
-    results_reu = db_cursor.fetchall()
+    try:
+        db_connection = get_db_connection()
+        db_cursor = db_connection.cursor()
+        
+        queryreu = "SELECT * FROM apidistribuicao.processo_reu WHERE ID_processo = %s"
+        db_cursor.execute(queryreu, (process_id,))
+        results_reu = db_cursor.fetchall()
 
-    reu_list = []
-    for reus in results_reu:
-        ID_reu= reus[1]
-        Cod_polo = reus[2]
-        nome = reus[3]
-        reu_list.append({'id_reu': ID_reu,
-                         'cod_polo': Cod_polo,
-                         'nomeReu':nome})
-    db_cursor.close()
-    db_connection.close()
+        reu_list = []
+        for reus in results_reu:
+            ID_reu= reus[1]
+            Cod_polo = reus[2]
+            nome = reus[3]
+            reu_list.append({'id_reu': ID_reu,
+                            'cod_polo': Cod_polo,
+                            'nomeReu':nome})
+        db_cursor.close()
+        db_connection.close()
 
-    return reu_list
+        return reu_list
+    except mysql.connector.Error as err:
+         logger.error(f"erro na consulta do Reu: {err}")
 
+#captura os numeros para envio
 def fetch_numero(cod_cliente):
+    try:
         db_connection = get_db_ligcontato()
         db_cursor_lig = db_connection.cursor()
-            #capturar numero para envio
+
         db_cursor_lig.execute("""SELECT nw.`number` FROM offices_whatsapp_numbers nw JOIN offices e ON nw.offices_id = e.offices_id 
-                              WHERE e.office_code = %s AND nw.status = 'L'""",(cod_cliente,))
+                              WHERE e.office_code = %s AND nw.status = 'L' AND nw.deleted = 0 """,(cod_cliente,))
         cliente_number = db_cursor_lig.fetchall()
         list_numbers = []
 
@@ -164,12 +177,15 @@ def fetch_numero(cod_cliente):
         db_cursor_lig.close()
 
         return list_numbers
+    except mysql.connector.Error as err:
+        logger.error(f"erro na consulta do numero de celular: {err}")
 
+#faz a verficação se o cliente esta ativo (L) ou Bloqueado (B)
 def validar_cliente(cod_cliente):
+    try:
         db_connection = get_db_connection()
         db_cursor = db_connection.cursor()
 
-        #faz a verficação se o cliente esta ativo (L) ou Bloqueado (B)
         db_cursor.execute("SELECT status FROM clientes WHERE Cod_escritorio = %s",(cod_cliente,))
         cliente_STATUS = db_cursor.fetchone()
 
@@ -177,14 +193,71 @@ def validar_cliente(cod_cliente):
         db_cursor.close()
 
         return cliente_STATUS
+    except mysql.connector.Error as err:
+        logger.error(f"erro na consulta do status do cliente: {err}")
 
+#captura emails para serem enviados 
 def fetch_email(cod_cliente):
-     
+     try:
         db_connection = get_db_ligcontato()
         db_cursor_lig = db_connection.cursor()
         #capturar email para envio
         db_cursor_lig.execute("""SELECT GROUP_CONCAT(DISTINCT oe.email ORDER BY oe.email SEPARATOR ', ') 
-                               FROM offices_emails oe JOIN offices e ON oe.offices_id = e.offices_id WHERE e.office_code = %s""",(cod_cliente,))
+                               FROM offices_emails oe JOIN offices e ON oe.offices_id = e.offices_id WHERE e.office_code = %s
+                              AND oe.status = 'L' AND oe.deleted = 0""",(cod_cliente,))
         email_cliente = db_cursor_lig.fetchone()
 
+        db_cursor_lig.close()
+        db_connection.close()
         return email_cliente
+     
+     except mysql.connector.Error as err:
+         logger.error(f"erro na consulta do email: {err}")
+         
+#puxa todos os dados necessario para envio de email e Whatsapp (SMTP/URL API)
+def fetch_companies():
+    try:
+        db_connection = get_db_connection()
+        db_cursor = db_connection.cursor()
+
+        # Puxar dados de configuração do companies
+        db_cursor.execute("""SELECT ID_lig, url_Sirius, sirius_Token,aws_s3_access_key,aws_s3_secret_key,bucket_s3,smtp_host, smtp_port, smtp_username, smtp_password, smtp_from_email, 
+                          smtp_from_name,smtp_reply_to, smtp_cc_emails,smtp_bcc_emails ,url_thumbnail_whatsapp, url_thumbnail FROM companies""")
+        config = db_cursor.fetchone()
+
+        db_cursor.close()
+        db_connection.close()
+
+        return config
+
+    except mysql.connector.Error as err:
+        logger.error(f"Erro na consulta do banco companies: {err}")
+        exit()
+
+#atualiza o status do processo de envio e insere no banco o email enviado
+def status_envio(processo_id,numero_processo,cod_escritorio,localizador_processo,
+                 data_do_dia,localizador_email,email_receiver,numero,permanent_url):
+    try:
+        db_connection = get_db_connection()
+        db_cursor = db_connection.cursor()
+
+        db_cursor.execute("UPDATE processo SET status = 'S' WHERE ID_processo = %s", (processo_id,))
+        db_cursor.execute("""INSERT INTO envio_emails (ID_processo, numero_processo, 
+                            cod_escritorio, localizador_processo, data_envio, localizador, email_envio, numero_envio, link_s3, data_hora_envio)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s,%s)""",
+                            (processo_id,   
+                            numero_processo,
+                            cod_escritorio,
+                            localizador_processo, 
+                            data_do_dia,
+                            localizador_email, 
+                            email_receiver,
+                            ','.join(cliente['numero'] for cliente in numero),
+                            permanent_url,
+                            datetime.now()))
+
+        db_connection.commit()
+        db_cursor.close()
+        db_connection.close()
+    except mysql.connector.Error as err:
+        logger.error(f"Erro ao atualizar o status ou registrar o envio: {err}")
